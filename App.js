af35d9972fd51f56f29141fee0caa92011be101d
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-12-22 10:57:51 +0700

    Add Header and Footer function

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <SafeAreaView />
+      <View
+        style={{
+          height: 70,
+          borderBottomWidth: 0.5,
+          borderBottomColor: "#E9E9E9",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontSize: 24 }}>Book Worm</Text>
+      </View>
+      <View style={{ flex: 1 }} />
+      <View
+        style={{
+          height: 70,
+          borderTopWidth: 0.5,
+          borderTopColor: "#E9E9E9",
+          flexDirection: "row",
+        }}
+      >
+        <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize:20}}>Total</Text>
+        </View>
+        <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize:20}}>Reading</Text>
+        </View>
+        <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize:20}}>Read</Text>
+        </View>
+      </View>
+      <SafeAreaView />
     </View>
   );
 }
@@ -14,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
